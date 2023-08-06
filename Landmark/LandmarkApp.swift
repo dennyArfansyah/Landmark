0a10339d95cd8372eac36aa77a87baa13917d818
dennyArfansyah <dennyarfansyah@gmail.com>
2023-08-06 21:21:18 +0700

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Denny Arfansyah on 06/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
