b56bf471a0f1ab929e202d0aa65d8b6a6450d8b7
dennyArfansyah <dennyarfansyah@gmail.com>
2023-08-08 16:10:49 +0700

    Adding toggle for show only favorite

--- Landmark/Views/LandmarkList.swift
+++ Landmark/Views/LandmarkList.swift
@@ -8,21 +8,26 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    @State private var showOnlyFavorite = true
+    @State private var showOnlyFavorite = false
     
     var filteredLandmarks: [Landmark] {
         landmarks.filter { landmark in
-            (showOnlyFavorite && landmark.isFavorite)
+            (showOnlyFavorite || landmark.isFavorite)
         }
     }
     
     var body: some View {
         NavigationView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showOnlyFavorite) {
+                    Text("Favorite Only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
