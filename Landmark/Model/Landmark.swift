7f51f706c163f2f09243fcf1c8f024d095376ed8
dennyArfansyah <dennyarfansyah@gmail.com>
2023-08-08 08:47:13 +0700

    Adding Model, dummy data and some resources

---
+++ Landmark/Model/Landmark.swift
@@ -0,0 +1,33 @@
+//
+//  Landmark.swift
+//  Landmark
+//
+//  Created by Denny Arfansyah on 08/08/23.
+//
+
+import CoreLocation
+import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var longitude: Double
+        var latitude: Double
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
+}
