004030e6ea6d85703b8f2fa7a508eb5b08a18d23
dennyArfansyah <dennyarfansyah@gmail.com>
2023-08-08 09:11:55 +0700

    Adding LandmarkDetail and navigation link in landmark list

---
+++ Landmark/Views/LandmarkDetail.swift
@@ -0,0 +1,46 @@
+//
+//  LandmarkDetail.swift
+//  Landmark
+//
+//  Created by Denny Arfansyah on 08/08/23.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Tutle Rock")
+                    .font(.title)
+                    .foregroundColor(.purple)
+                    .multilineTextAlignment(.center)
+                HStack {
+                    Text("Joshua True National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here")
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
