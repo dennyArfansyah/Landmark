0425659c1c6736013b20357a43d9ae4bc3e70c69
dennyArfansyah <dennyarfansyah@gmail.com>
2023-08-08 08:55:25 +0700

    Creating landmark row

---
+++ Landmark/Views/Cell/LandmarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkRow.swift
+//  Landmark
+//∏
+//  Created by Denny Arfansyah on 08/08/23.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[2])
+    }
+}
