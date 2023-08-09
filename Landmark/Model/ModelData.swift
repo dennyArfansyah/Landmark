3b555ce93372d27e32f47ce05a55ea0f9f831cde
dennyArfansyah <dennyarfansyah@gmail.com>
2023-08-10 00:47:07 +0700

    Finalizing Tab Featured and List

--- Landmark/Model/ModelData.swift
+++ Landmark/Model/ModelData.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
+final class ModelData: ObservableObject {
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
+    
+    var featured: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
+}
+
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
     
@@ -26,17 +42,5 @@ func load<T: Decodable>(_ filename: String) -> T {
         return try decoder.decode(T.self, from: data)
     } catch {
         fatalError("Could not parse \(filename) as \(T.self):\n\(error)")
-    }
-}
-
-final class ModelData: ObservableObject {
-    @Published var landmarks: [Landmark] = load("landmarkData.json")
-    var hikes: [Hike] = load("hikeData.json")
-    
-    var categories: [String: [Landmark]] {
-        Dictionary(
-            grouping: landmarks,
-            by: { $0.category.rawValue }
-        )
     }
 }
